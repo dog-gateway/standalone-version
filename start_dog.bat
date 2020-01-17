9b92b60eacbc82f5dd77c52a57e3bccd3c0855c2
Dario Bonino <dario.bonino@gmail.com>
2020-01-17 15:30:57 +0100

    New standalone version, used as a base for the Debian packages.

--- start_dog.bat
+++ start_dog.bat
@@ -1,3 +1,4 @@
-java -Dorg.osgi.service.http.port=8080 -DconfigFolder=configuration/config -jar org.eclipse.osgi_3.10.1.v20140909-1633.jar -console -clean
+java -Dorg.osgi.service.http.port=8080 -DconfigFolder=configuration/config -jar org.eclipse.osgi_3.15.100.v20191114-1701.jar
+ -console -clean
 
 
