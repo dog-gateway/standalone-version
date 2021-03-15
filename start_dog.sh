3f55e00ce1fe5c739338be1ef1e7ff3812d74a56
dbonino <dario.bonino@gmail.com>
2021-03-15 10:12:03 +0100

    Updated dependencies to the latest Eclipse Equinox.

--- start_dog.sh
+++ start_dog.sh
@@ -1,1 +1,3 @@
-java -Xmx128m -Dorg.osgi.service.http.port=8080 -DconfigFolder=./configuration/config -Dgnu.io.rxtx.SerialPorts=/dev/ttyUSB0:/dev/ttyUSB1:/dev/ttyUSB2:/dev/ttyUSB3 -jar org.eclipse.osgi_3.15.300.v20200520-1959.jar -clean -console 
+java -Xmx128m -Dorg.osgi.service.http.port=8080 -DconfigFolder=/etc/exacto/config  -Dgnu.io.rxtx.SerialPorts=/dev/ttyS0:/dev/ttyUSB0:/dev/ttyUSB1:/dev/ttyUSB2:/dev/ttyUSB3 -jar org.eclipse.osgi_3.16.100.v20201030-1916.jar -clean -console 
+
+
