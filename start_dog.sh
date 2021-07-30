b5a315dfc77c3a669da3a0ba4846cd37062417d7
dbonino <dario.bonino@gmail.com>
2021-07-30 12:01:08 +0200

    Upgraded to latest equinox version.

--- start_dog.sh
+++ start_dog.sh
@@ -1,3 +1,3 @@
-java -Xmx128m -Dorg.osgi.service.http.port=8080 -DconfigFolder=/etc/exacto/config  -Dgnu.io.rxtx.SerialPorts=/dev/ttyS0:/dev/ttyUSB0:/dev/ttyUSB1:/dev/ttyUSB2:/dev/ttyUSB3 -jar org.eclipse.osgi_3.16.200.v20210226-1447.jar -clean -console 
+java -Xmx128m -Dorg.osgi.service.http.port=8080 -DconfigFolder=/etc/exacto/config  -Dgnu.io.rxtx.SerialPorts=/dev/ttyS0:/dev/ttyUSB0:/dev/ttyUSB1:/dev/ttyUSB2:/dev/ttyUSB3 -jar org.eclipse.osgi_3.16.300.v20210525-1715.jar -clean -console 
 
 
