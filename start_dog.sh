56729746eb8a2d7e2905c8ae360aadc2f00a2ef8
dbonino <dario.bonino@gmail.com>
2022-10-28 17:04:43 +0200

    Updated standalone version

--- start_dog.sh
+++ start_dog.sh
@@ -1,3 +1,3 @@
-java -Xmx128m -Dorg.osgi.service.http.port=8080 -DconfigFolder=/etc/exacto/config  -Dgnu.io.rxtx.SerialPorts=/dev/ttyS0:/dev/ttyUSB0:/dev/ttyUSB1:/dev/ttyUSB2:/dev/ttyUSB3 -jar org.eclipse.osgi_3.17.100.v20211104-1730.jar -clean -console 
+java -Xmx128m -Dorg.osgi.service.http.port=8080 -DconfigFolder=/etc/dog/config -Dgnu.io.rxtx.SerialPorts=/dev/ttyUSB0:/dev/ttyUSB1:/dev/ttyUSB2:/dev/ttyUSB3:/dev/ttyS0 -jar org.eclipse.osgi_3.18.100.v20220817-1601.jar -clean -console 
 
 
